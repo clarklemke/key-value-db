98cce2f4981b3d8656e4934fd91ab68b36b3ad06
clarklemke <clark.lemke@gmail.com>
2022-02-15 13:21:58 -0800

    added typing

--- keyvaluedb.py
+++ keyvaluedb.py
@@ -1,19 +1,16 @@
+from typing import Union
+
 import os
 
 
-def load(location):
-    """Return a database object and import data in location"""
-    return KeyValueDB(location)
-
-
 class KeyValueDB:
-    def __init__(self, location):
+    def __init__(self, location: str) -> None:
         """Creates a database object and loads the data from the location path.
         If the file does not exist it will be created on the first update.
         """
         self.load(location)
 
-    def load(self, location):
+    def load(self, location: str) -> bool:
         """Loads the data file"""
         location = os.path.expanduser(location)
         if os.path.exists(location):
@@ -23,7 +20,7 @@ class KeyValueDB:
             self.db = {}
         return True
 
-    def _load_db(self, location):
+    def _load_db(self, location: str) -> None:
         """Helper function to load data file"""
         self.db = {}
         with open(location) as f:
@@ -31,27 +28,32 @@ class KeyValueDB:
                 key, *values = line.split()
                 self.db[key] = " ".join(values)
 
-    def get(self, key):
+    def get(self, key: str) -> Union[str, bool]:
         """Get the value of a key"""
         try:
             return self.db[key]
         except KeyError:
             return False
 
-    def get_all(self):
+    def get_all(self) -> list:
         """Return a list of all keys in db"""
         return self.db.keys()
 
-    def exists(self, key):
+    def exists(self, key: str) -> bool:
         """Return True if key exists in db, return False if not"""
         return key in self.db
 
-    def total_keys(self):
+    def total_keys(self) -> int:
         """Get a total number of keys in db"""
         total = len(self.db)
         return total
 
-    def del_db(self):
+    def del_db(self) -> bool:
         """Delete everything from the database"""
         self.db = {}
         return True
+
+
+def load(location: str) -> KeyValueDB:
+    """Return a database object and import data in location"""
+    return KeyValueDB(location)
